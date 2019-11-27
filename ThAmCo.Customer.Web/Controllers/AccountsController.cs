864b567106a2061e0152e769696950fdc6185d0a
calpayne <34384159+calpayne@users.noreply.github.com>
2019-11-27 20:10:19 +0000

    Added basic boiler code for accounts (login, register, edit)

---
+++ ThAmCo.Customer.Web/Controllers/AccountsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThAmCo.Customer.Web.Models;
+
+namespace ThAmCo.Customer.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login([Bind("Username,Password")] LoginViewModel lvm)
+        {
+            if (ModelState.IsValid)
+            {
+                //do stuff
+                return RedirectToAction(nameof(Login));
+            }
+
+            return View(lvm);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register([Bind("Username,EmailAddress,Password")] RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                //do stuff
+                return RedirectToAction(nameof(Register));
+            }
+
+            return View(rvm);
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("FirstName,Surname,Email,TelNo,DeliverAddress1,DeliverAddress2,DeliverAddress3,Postcode")] UpdateViewModel uvm)
+        {
+            if (ModelState.IsValid)
+            {
+                //do stuff
+                return RedirectToAction(nameof(Update));
+            }
+
+            return View(uvm);
+        }
+    }
+}
---
+++ ThAmCo.Customer.Web/Models/UpdateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Customer.Web.Models
+{
+    public class UpdateViewModel
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string TelNo { get; set; }
+        public string DeliverAddress1 { get; set; }
+        public string DeliverAddress2 { get; set; }
+        public string DeliverAddress3 { get; set; }
+        public string Postcode { get; set; }
+    }
+}
