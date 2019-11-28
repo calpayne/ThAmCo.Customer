5fb632d612d3146c773ffa17788862cc1d378272
calpayne <34384159+calpayne@users.noreply.github.com>
2019-11-28 19:37:56 +0000

    Added setup for real reviews service

---
+++ ThAmCo.Customer.Services/Reviews/ReviewsService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ThAmCo.Customer.Models;
+
+namespace ThAmCo.Customer.Services.Reviews
+{
+    public class ReviewsService : IReviewsService
+    {
+        private readonly HttpClient _client;
+
+        public ReviewsService(IConfiguration config, HttpClient client)
+        {
+            client.BaseAddress = new System.Uri(config.GetConnectionString("Reviews"));
+            client.Timeout = TimeSpan.FromSeconds(5);
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            _client = client;
+        }
+
+        public async Task<IEnumerable<ReviewDto>> GetAllAsync(int id)
+        {
+            IEnumerable <ReviewDto> review;
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync("/api/reviews/" + id);
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                response.EnsureSuccessStatusCode();
+
+                review = await response.Content.ReadAsAsync< IEnumerable<ReviewDto>>();
+            }
+            catch (HttpRequestException)
+            {
+                review = Array.Empty<ReviewDto>();
+            }
+
+            return review;
+        }
+    }
+}
