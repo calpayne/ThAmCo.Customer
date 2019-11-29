73b66289dc467df8c2ea7482201d0244da4b0d92
Cal Payne <t7076908@live.tees.ac.uk>
2019-11-29 10:25:41 +0000

    Created base code for profile service

---
+++ ThAmCo.Customer.Models/ProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThAmCo.Customer.Models
+{
+    public class ProfileDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string TelNo { get; set; }
+        public string DeliverAddress1 { get; set; }
+        public string DeliverAddress2 { get; set; }
+        public string DeliverAddress3 { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ ThAmCo.Customer.Services/Profiles/IProfilesService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ThAmCo.Customer.Models;
+
+namespace ThAmCo.Customer.Services.Profiles
+{
+    public interface IProfilesService
+    {
+        Task<bool> UpdateProfileAsync(ProfileDto profile);
+        Task<ProfileDto> GetProfileAsync(int id);
+    }
+}
