99120e771b178fd2999255c3eb09b477b3b34464
calpayne <34384159+calpayne@users.noreply.github.com>
2019-12-16 19:46:41 +0000

    Added fake profiles service

---
+++ ThAmCo.Customer.Services/Profiles/FakeProfilesService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThAmCo.Customer.Models;
+
+namespace ThAmCo.Customer.Services.Profiles
+{
+    public class FakeProfilesService : IProfilesService
+    {
+        private IEnumerable<ProfileDto> _profiles;
+
+        public FakeProfilesService()
+        {
+            _profiles = new List<ProfileDto>
+            {
+                new ProfileDto { Id = 1, DeliverAddress1 = "DeliverAddress1", DeliverAddress2 = "DeliverAddress2", DeliverAddress3 = "DeliverAddress3", Email = "email@email.com", FirstName = "First", Postcode = "Postcode", Surname = "Sur", TelNo = "1234" }
+            };
+        }
+
+        public Task<ProfileDto> GetProfileAsync(int id)
+        {
+            return Task.FromResult(_profiles.FirstOrDefault(p => p.Id == id));
+        }
+
+        public Task<bool> UpdateProfileAsync(ProfileDto profile)
+        {
+            var data = _profiles.FirstOrDefault(p => p.Id == profile.Id);
+
+            if (data == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            data = profile;
+            return Task.FromResult(true);
+        }
+    }
+}
