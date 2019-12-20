8f3a687aca2fa73fa0f0bddc4e94cd2b902f57b9
calpayne <34384159+calpayne@users.noreply.github.com>
2019-12-20 22:13:29 +0000

    Added more accounts unit tests

--- ThAmCo.Customer.Tests/AccountTests.cs
+++ ThAmCo.Customer.Tests/AccountTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using ThAmCo.Customer.Models;
 using ThAmCo.Customer.Services.Auth;
 using ThAmCo.Customer.Services.Profiles;
 using ThAmCo.Customer.Web.Controllers;
@@ -132,6 +134,65 @@ namespace ThAmCo.Customer.Tests
             // Assert
             Assert.IsNotNull(result);
             var objResult = result as BadRequestResult;
+            Assert.IsNotNull(objResult);
+        }
+
+        [TestMethod]
+        public async Task LoggingIn_InvalidLogin_ShouldLocalRedirect()
+        {
+            // Arrange
+            var authServiceMock = new Mock<IAuthenticationService>();
+            authServiceMock.Setup(_ => _.SignInAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<ClaimsPrincipal>(), It.IsAny<AuthenticationProperties>()))
+                           .Returns(Task.FromResult((object)null));
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock.Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                               .Returns(authServiceMock.Object);
+
+            var controller = new AccountController(new FakeProfilesService(), new FakeAuthService())
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        RequestServices = serviceProviderMock.Object
+                    }
+                }
+            };
+
+            // Act
+            var result = await controller.Login(new LoginViewModel
+            {
+                Email = "example@email.com",
+                Password = "Password"
+            });
+
+            // Assert
+            Assert.IsNotNull(result);
+            var objResult = result as LocalRedirectResult;
+            Assert.IsNotNull(objResult);
+        }
+
+        [TestMethod]
+        public void LoggingIn_WhenAlreadyLoggedIn_ShouldLocalRedirect()
+        {
+            // Arrange
+            var controller = new AccountController(new FakeProfilesService(), new FakeAuthService());
+
+            var contextMock = new Mock<HttpContext>();
+            contextMock.Setup(ctx => ctx.User.Identity.IsAuthenticated).Returns(true);
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = contextMock.Object
+            };
+
+            // Act
+            var result = controller.Login();
+
+            // Assert
+            Assert.IsNotNull(result);
+            var objResult = result as LocalRedirectResult;
             Assert.IsNotNull(objResult);
         }
     }
