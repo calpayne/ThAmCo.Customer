0385a2a310d7c1383ca06949e652ed98a819eea7
calpayne <34384159+calpayne@users.noreply.github.com>
2019-12-31 19:38:21 +0000

    Added unit tests for requesting account deletion

--- ThAmCo.Customer.Tests/AccountTests.cs
+++ ThAmCo.Customer.Tests/AccountTests.cs
@@ -196,5 +196,54 @@ namespace ThAmCo.Customer.Tests
             var objResult = result as LocalRedirectResult;
             Assert.IsNotNull(objResult);
         }
+
+        [TestMethod]
+        public async Task RequestDeletion_LoggedIn_ShouldRedirectToAction()
+        {
+            // Arrange
+            IEnumerable<Claim> fakeClaims = new List<Claim>
+            {
+                new Claim("sub", "f32d935b-f175-4450-a93e-e48711c4d481"),
+                new Claim("preferred_username", "email@example.com"),
+                new Claim("name", "Mr Esting")
+            };
+
+            var claimsMock = new Mock<ClaimsPrincipal>();
+            claimsMock.Setup(m => m.Claims).Returns(fakeClaims);
+
+            var controller = new AccountController(new FakeProfilesService(), new FakeAuthService());
+
+            var contextMock = new Mock<HttpContext>();
+            contextMock.Setup(ctx => ctx.User).Returns(claimsMock.Object);
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = contextMock.Object
+            };
+
+            // Act
+            var result = await controller.RequestDeletion();
+
+            // Assert
+            Assert.IsNotNull(result);
+            var objResult = result as RedirectToActionResult;
+            Assert.IsNotNull(objResult);
+            Assert.AreEqual(objResult.ActionName, "Logout");
+        }
+
+        [TestMethod]
+        public async Task RequestDeletion_NotLoggedIn_ShouldBadRequest()
+        {
+            // Arrange
+            var controller = new AccountController(new FakeProfilesService(), new FakeAuthService());
+
+            // Act
+            var result = await controller.RequestDeletion();
+
+            // Assert
+            Assert.IsNotNull(result);
+            var objResult = result as BadRequestResult;
+            Assert.IsNotNull(objResult);
+        }
     }
 }
